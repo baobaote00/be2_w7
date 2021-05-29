5fca1cfbf4fe8d695ef47c6472a339fd6a78fe3b
baobaote00 <baobaote00@gmail.com>
2021-05-29 09:41:55 +0700

    include

--- week3/resources/views/companies.blade.php
+++ week3/resources/views/companies.blade.php
@@ -31,14 +31,7 @@
 </head>
 
 <body>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-        <h5 class="my-0 mr-md-auto font-weight-normal">Company name</h5>
-        <nav class="my-2 my-md-0 mr-md-3">
-            <a class="p-2 text-dark" href="{{ url('/companies') }}">Companies</a>
-            <a class="p-2 text-dark" href="{{ url('/trainer') }}">Trainer</a>
-            <a class="p-2 text-dark" href="{{ url('/search') }}">Search</a>
-        </nav>
-    </div>
+    @include('header')
 
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
         <h1 class="display-4">Companies</h1>
